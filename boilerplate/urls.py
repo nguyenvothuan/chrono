50825b4205183b8a3342b93135f5cfbfc0e32d61
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-03-27 18:03:52 -0400

    fix shorter url

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -21,7 +21,7 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('api/v1.0/boilerplate_apps/',
+                  path('',
                        include("boilerplate_app.urls", namespace="boilerplate_app-api")),
                   path("graphql/", GraphQLView.as_view(graphiql=True)),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
