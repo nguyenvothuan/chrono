d1d0276105825863456e94677ee5052c3541dc4f
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-03-27 18:46:04 -0400

    finish account user

--- account/models.py
+++ account/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-
+from boilerplate_app.models import User
+from datetime import datetime
 # Create your models here.
-class User(models.Model):
+class Account(models.Model):
     id = models.IntegerField(default=0, primary_key=True)
+    manager = models.ForeignKey('Account', on_delete=models.CASCADE, blank=True, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, default=True)
+    positionTitle = models.CharField(max_length=100, blank=True, default="")
+    startDate = models.DateField(default=datetime.now())
+    isManager = models.BooleanField(default=False)
+    
