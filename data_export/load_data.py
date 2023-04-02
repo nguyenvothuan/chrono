d4aa8e28ccc812bf3e02c73153583cf962a62c49
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-04-02 00:55:31 -0400

    dump data, but there is a bug:

--- data_export/load_data.py
+++ data_export/load_data.py
@@ -1,9 +1,12 @@
+from django.db import IntegrityError
 from account.models import Account
 from boilerplate_app.models import User
 from company.models import Company
-from util import get_company_employee_id
+from time_entry.models import TimeEntry
+from data_export.util import get_company_employee_id, get_account_id, compute_hours_between_clock_times
 import json
 import datetime
+
 
 def dump_employee():
     for file in ["Onion_Technology-employees", "GizmoGram-employees", "LunchRock_LLC-employees", "Night_Owls_Inc-employees"]:
@@ -60,9 +63,53 @@ def dump_manager():
     pass
 
 
-def dump_time_series():
-    for file in ["GizmoGram-employees"]:
+def dump_time_entries():
+    for file in ["Onion_Technology", "Night_Owls_Inc", "LunchRock_LLC", "GizmoGram"]:
         with open(f"data_export/{file}-time-entries.json") as f:
             time_entries = json.load(f)
+            companyId = 1
+            if file == 'Onion_Technology':
+                companyId = 1
+            elif file == 'LunchRock_LLC':
+                companyId = 2
+            elif file == 'Night_Owls_Inc':
+                companyId = 3
+            else:
+                companyId = 4
             for time_entry in time_entries:
-                print(time_entry['employeeId'], time_entry['companyId'])
+                accountId = get_account_id(
+                    company_id=companyId, employee_id=time_entry['employeeId'])
+                account = Account.objects.get(pk=accountId)
+                for entry in time_entry.get('timeEntries'):
+                    # compute the hoursWorked here:
+                    hoursWorked = 0
+                    date = datetime.date.today()
+                    if file == 'GizmoGram':
+                        hoursWorked = entry.get('hoursWorked')
+                        date = datetime.datetime.strptime(
+                            entry.get("date"), '%Y-%m-%d').date()
+                    elif file in ['LunchRock_LLC', 'Night_Owls_Inc']:
+                        hoursWorked = compute_hours_between_clock_times(
+                            entry.get('clockedIn'), entry.get('clockedOut'))
+                        date = datetime.datetime.strptime(
+                            entry.get("date"), '%Y-%m-%d').date()
+                    elif file == 'Onion_Technology':
+                        hoursWorked = compute_hours_between_clock_times(entry.get(
+                            'clockedInEpochMillisecond'), entry.get('clockedOutEpochMillisecond'))
+                        date = datetime.date.fromtimestamp(
+                            entry.get('clockedInEpochMillisecond')/1000)
+                    #handle case when there are two clock in clock out a day
+                    try:
+                        newEntry, _ = TimeEntry.objects.get_or_create(
+                            date=date, account=account, hoursWorked=hoursWorked)
+                    except IntegrityError as e:
+                        print(account, date)
+                        print(e)
+
+
+def flush_time_entries():
+    TimeEntry.objects.all().delete()
+
+
+# dump_employee()
+dump_time_entries()
